e715d85d90544bf4ee746c5fd615c28ffc18cb1e
HDS <hedongshu@foxmail.com>
2021-11-11 10:27:44 +0800

    Update index.d.ts
    mix?: number;  =====>   max?: number;

--- index.d.ts
+++ index.d.ts
@@ -24,7 +24,7 @@ interface RulesType {
         default?: any; // The default value of property, once the property is allowed non-required and missed, parameter will use this as the default value. This may change the original input params
         widelyUndefined?: any; // override options.widelyUndefined
         min?: number;
-        mix?: number;
+        max?: number;
         allowEmpty?: boolean; //allow empty string, default to false. If rule.required set to false, allowEmpty will be set to true by default.
         format?: RegExp; // A RegExp to check string's format.
         trim?: boolean; // Trim the string before check, default is false
